25625dcf3e40080b8461c002e8f700e89ffbfdc3
gunadeeptulimelli <63964428+gunadeeptulimelli@users.noreply.github.com>
2020-11-21 08:54:23 +0530

    MultipleQuestion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,107 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'interview';
+  questions:any;
+ currentIndex:number;
+ notAttempted:any;
+ correct:any;
+ currentQuestionSet:any;
+ start=false;
+ gameover=false;
+ buttonname="Play";
+  constructor(){
+    this.questions=[
+      {
+        id:1,
+        question:'What is the full form of IP?',
+        option:[
+          {optionid:1,name:'Internet program'},
+          {optionid:2,name:'Internet protocol'},
+          {optionid:3,name:'Interface program'},
+          {optionid:4,name:'Interface protocol'}
+        ],
+        answer:2,
+        selected:0
+      },
+      {
+        id:2,
+        question:'Select the smallest memory size',
+        option:[
+          {optionid:1,name:'kilobyte'},
+          {optionid:2,name:'megabyte'},
+          {optionid:3,name:'gigabyte'},
+          {optionid:4,name:'terabyte'}
+        ],
+        answer:1,
+        selected:0
+      },
+      {
+        id:3,
+        question:'What is the full form of IP?',
+        option:[
+          {optionid:1,name:'Internet program'},
+          {optionid:2,name:'Internet protocol'},
+          {optionid:3,name:'Interface program'},
+          {optionid:4,name:'Interface protocol'}
+        ],
+        answer:2,
+        selected:0
+      },
+      {
+        id:4,
+        question:'Select the smallest memory size',
+        option:[
+          {optionid:1,name:'kilobyte'},
+          {optionid:2,name:'megabyte'},
+          {optionid:3,name:'gigabyte'},
+          {optionid:4,name:'terabyte'}
+        ],
+        answer:1,
+        selected:0
+      }
+    ];
+ 
+    this.currentIndex=0;
+    this.currentQuestionSet= this.questions[this.currentIndex];
+  }
+ 
+
+ 
+  next(){
+    this.currentIndex = this.currentIndex + 1;
+    this.currentQuestionSet= this.questions[this.currentIndex];
+  }
+  prev(){
+    if(this.currentIndex > -1){
+      this.currentIndex = this.currentIndex - 1;
+      this.currentQuestionSet= this.questions[this.currentIndex];
+    } 
+  }
+  submit(){
+    this.buttonname="Replay";
+    if(this.currentIndex+1==this.questions.length){
+       this.gameover=true;
+       this.start=false;
+         this.correct=0;
+    this.notAttempted=0;
+    this.questions.map(x=>{
+        if(x.selected!=0){
+          if(x.selected == x.answer)
+            this.correct=this.correct + 1;
+        }
+        else{
+          this.notAttempted=this.notAttempted + 1;
+        }
+        x.selected=0;
+    });
+    }
+  
+  }
+  startQuiz(){
+    this.gameover=false;
+    this.currentIndex=0;
+   this.currentQuestionSet= this.questions[this.currentIndex];
+      this.start=true;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
